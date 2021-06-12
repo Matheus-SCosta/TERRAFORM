ebc53eb64cb2de73569f867c8629320cf5a85af7
Matheus Costa <m.costa@njp006476.conductor.tecnologia>
2021-06-11 21:41:24 -0300

    ajustando repositorio

---
+++ AWS/Route_53/main.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::##########:role/TerraformRole" 
+    session_name = "terraform_cross_account_access"
+  }
+
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-backend-new"
+    key            = "r53/ambiente-lab.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_state"
+    role_arn = "arn:aws:iam::##########:role/TerraformRole" 
+  }
+}
+
+// Recursos são chamados logo abaixo
+
+resource "aws_route53_zone" "ambiente_lab" {
+  name = "ambiente.lab"
+  comment = "Novo dns para o ambiente de lab"
+
+  vpc {
+    vpc_id = data.aws_vpc.selected.id
+  }
+}
+
+
+resource "aws_route53_record" "dbesp" {
+  name    = "dbesp.ambiente.lab"
+  type    = "A"
+  ttl     = "300"
+  zone_id = aws_route53_zone.ambiente_lab.zone_id
+  records = ["10.10.3.99"]
+  depends_on = [
+    aws_route53_zone.ambiente_lab
+  ]
+}
+
+resource "aws_route53_record" "dbhml" {
+  name    = "dbhml.ambiente.lab"
+  type    = "A"
+  ttl     = "300"
+  zone_id = aws_route53_zone.ambiente_lab.zone_id
+  records = ["10.10.3.10"]
+  depends_on = [
+    aws_route53_zone.ambiente_lab
+  ]
+}
+
+resource "aws_route53_record" "dbm" {
+  name    = "dbm.ambiente.lab"
+  type    = "A"
+  ttl     = "300"
+  zone_id = aws_route53_zone.ambiente_lab.zone_id
+  records = ["10.10.3.57"]
+  depends_on = [
+    aws_route53_zone.ambiente_lab
+  ]
+}
+
+resource "aws_route53_record" "dbtst" {
+  name    = "dbtst.ambiente.lab"
+  type    = "A"
+  ttl     = "300"
+  zone_id = aws_route53_zone.ambiente_lab.zone_id
+  records = ["10.10.5.23"]
+  depends_on = [
+    aws_route53_zone.ambiente_lab
+  ]
+}
+
+resource "aws_route53_record" "poc" {
+  name    = "poc.ambiente.lab"
+  type    = "A"
+  ttl     = "300"
+  zone_id = aws_route53_zone.ambiente_lab.zone_id
+  records = ["172.16.0.7"]
+  depends_on = [
+    aws_route53_zone.ambiente_lab
+  ]
+}
+
