a8fc9eb36efc4007458ddcd3caf307f5befe2659
Matheus-SCosta <silvadacosta2018@gmail.com>
2022-03-14 11:37:51 -0300

    backend

--- AWS/configurando_role_terraform/s3.tf
+++ AWS/configurando_role_terraform/s3.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_object" "object" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("/home/local/CONDUCTOR/m.silva/Documentos/VSCODE/${local.name_file}")
+  #etag = filemd5("/home/local/CONDUCTOR/m.silva/Documentos/VSCODE/${local.name_file}")
 }
 
 
